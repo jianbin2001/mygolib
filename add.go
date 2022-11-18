af448fbe613388dda67c457272d31186a8aa7fdb
chenjb <chenjb@getui.com>
2022-11-18 20:37:37 +0800

    init

---
+++ add.go
@@ -0,0 +1,9 @@
+package mylib
+
+func Add(a int64, b int64) int64 {
+	return a + b
+}
+
+func Sub(a int64, b int64) int64 {
+	return a - b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jianbin_2001/mylib
+
+go 1.19
